c76699df71f6c5dff58d7905a5596e3191588b31
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-01-27 11:50:39 +0000

    refactor

---
+++ engine-new/engine-new/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace engine_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
